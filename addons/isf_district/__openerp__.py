e995a69c81a2fb3b00824bb30369bf00205ff6b0
Matteo Angelino <matteo.angelino@gmail.com>
2013-09-09 09:01:37 +0200

    Add description

--- addons/isf_district/__openerp__.py
+++ addons/isf_district/__openerp__.py
@@ -4,7 +4,18 @@
 	'name': 'Manage District',
 	'version': '1.0',
 	'category': 'Tools',
-	'description': """District management""",
+	'description': """
+District management
+===================
+
+Adding new functionality to manage district, zone and sub zones. From the new menu
+'Manage District' the user can:
+
+* Define a new district or modify one
+* Define a new zone to the district
+* Define a sub zone for a couple (District, zone)
+* Define a plot for the tuple <District, zone, subzone>
+""",
 	'author': 'Matteo Angelino (matteo.angelino@informaticisenzafrontiere.org)',
 	'website': 'www.informaticisenzafrontiere.org',
 	'license': 'AGPL-3',
